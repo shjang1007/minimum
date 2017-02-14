4777f54048aaaeb50eb1502084298e6ee2115bcb
Brian Jang <brian.jang91@gmail.com>
2017-02-14 15:07:23 -0500

    Add session reducers and store configuration

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { merge } from "lodash";
+
+import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from "../actions/session_actions";
+
+const _initialState = {
+  currentUser: null,
+  errors: []
+};
+
+const sessionReducer = (oldState = _initialState, action) => {
+  // Keep state immutable
+  Object.freeze(oldState);
+
+  switch (action.type) {
+    case RECEIVE_CURRENT_USER:
+      return {
+        currentUser: action.currentUser,
+        errors: []
+      };
+    case RECEIVE_ERRORS:
+      return {
+        currentUser: null,
+        errors: action.errors
+      };
+    default:
+      return oldState;
+  }
+};
+
+export default sessionReducer;
