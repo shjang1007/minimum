65797c8e6a3e4b27d4e6b6408aefc7ac25d93133
Brian Jang <brian.jang91@gmail.com>
2017-02-14 14:53:21 -0500

    Add session actions to handle sign up/in/out

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,37 @@
+import * as sessionApiUtil from "../util/session_api_util";
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+export const signup = (user) => (dispatch) => {
+  return sessionApiUtil.signup(user).then(
+    (user) => (dispatch(receiveCurrentUser(user)))
+  );
+};
+
+export const signin = (user) => (dispatch) => {
+  return sessionApiUtil.signin(user).then(
+    (user) => (dispatch(receiveCurrentUser(user)))
+  );
+};
+
+export const signout = () => (dispatch) => {
+  return sessionApiUtil.signout().then(
+    () => (dispatch(receiveCurrentUser(null)))
+  );
+};
+
+
+export const receiveCurrentUser = (currentUser) => {
+  return ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+  });
+};
+
+export const receiveErrors = (errors) => {
+  return ({
+    type: RECEIVE_ERRORS,
+    errors
+  });
+};
