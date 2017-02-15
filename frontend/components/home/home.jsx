89824ce07ee4fcf8ccc44df5ce82647d4a1d9bbd
Brian Jang <brian.jang91@gmail.com>
2017-02-14 22:56:35 -0500

    Stop for the end of day, in the middle of css styling

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,28 +1,65 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router";
 
-const Home = ({ currentUser, signout}) => {
-    return (
-      <header className="main-nav">
-        <nav className="left-nav">
-          <Link to="/"><i className="fa fa-medium" aria-hidden="true"></i> Medium</Link>
-        </nav>
+import SessionFormContainer from "../session/session_form_container";
 
-        <nav className="right-nav">
+export default class Home extends Component {
+  toggleModalActive(e) {
+    e.preventDefault();
+    let modal = document.getElementById("sign-modal");
+    modal.addClass("is-active");
+  }
+
+  render() {
+    return (
+      <main>
+        <header className="main-bar">
+          <div className="inner-bar">
+            <nav className="left-nav">
+              <Link to="/" className="logo-link">
+                <img src="/assets/konoha-logo.png" className="logo" />
+                <span>Medium</span>
+
+              </Link>
+            </nav>
+
+            <nav className="right-nav">
+              <ul>
+                <li>
+                  <button className=" button gray-button">Write a story</button>
+                </li>
+                <li>
+                  <Link to="/signup" className="button green-button">Sign up</Link>
+                </li>
+                <li>
+                  <Link to="/signin" className="button green-button">Sign in</Link>
+                </li>
+                <li>
+                  <button className="button gray-button">
+                    <i className="fa fa-search" aria-hidden="true"></i>
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="inner-bar">
+            <ul>
+              <li>Home</li>
+              <li>Top stories</li>
+              <li>Editors'' picks</li>
+            </ul>
+          </div>
+        </header>
+
+        <section className="main-body">
           <ul>
-            <li><button className=" button gray-button">Write a story</button></li>
-            <li><Link to="/signup" className="button green-button">Sign up</Link></li>
-            <li><Link to="/signin" className="button green-button">Sign in</Link></li>
             <li>
-              <button className="button gray-button">
-                <i className="fa fa-search" aria-hidden="true"></i>
-              </button>
+              <p>AHHHHHHH</p>
             </li>
           </ul>
+        </section>
+      </main>
 
-        </nav>
-      </header>
-    )
-};
-
-export default Home;
+    );
+  }
+}
