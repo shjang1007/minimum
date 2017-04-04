1689bee562d0e5a9224376f42c4ba635b10f51d0
Brian Jang <brian.jang91@gmail.com>
2017-04-04 11:13:02 -0400

    Fix profile view error

--- frontend/components/user/user_show_detail/user_show_detail.jsx
+++ frontend/components/user/user_show_detail/user_show_detail.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const UserShowDetail = ({ user, currentUser, toggleForm }) => {
   let editButton = null;
-  if (user.id === currentUser.id) {
+  if (currentUser && user.id === currentUser.id) {
     editButton = <button type="button" onClick={ toggleForm }>Edit</button>;
   }
   // Add {editButton} in between line 19 and 20. For production pushing without it
