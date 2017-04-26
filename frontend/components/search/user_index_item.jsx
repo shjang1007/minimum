c14a0363fd427ebba5131e256fab6d6b902d77a8
Brian Jang <brian.jang91@gmail.com>
2017-04-26 16:44:16 -0400

    Add user list to the search and add styles

--- frontend/components/search/user_index_item.jsx
+++ frontend/components/search/user_index_item.jsx
@@ -1,17 +1,29 @@
 import React, { Component } from "react";
-import { Link } from "react-router";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router";
+import { fetchUser } from "../../actions/user_actions";
 
 // Props has story info
 class UserIndexItem extends Component {
+  handleNavigate(e) {
+    const { fetchUser, user, router } = this.props;
+    e.preventDefault();
+
+    fetchUser(user.username).then(
+      action => {
+        window.scrollTo(0,0);
+        router.push(`/@${user.username}`);
+      }
+    );
+  }
+
   renderDescription() {
     const { description } = this.props.user;
     if (description) {
       return(
-        <li>
-          <p className="index-item-content">
-            { description }
-          </p>
-        </li>
+        <p className="index-item-content search-content">
+          { description }
+        </p>
       );
     }
   }
@@ -20,22 +32,22 @@ class UserIndexItem extends Component {
     const { id, username, name, description, avatar_url } = this.props.user;
 
     return(
-      <li className="index-item">
+      <li className="index-item search-index-item">
         <div className="index-item-profile">
           <ul className="index-item-author-info">
             <li>
-              <Link to={ `/@${username}` }>
+              <button onClick={ this.handleNavigate.bind(this) }>
                 <img src={ avatar_url }
-                    className="story-avatar avatar" />
-              </Link>
+                    className="story-avatar avatar search-avatar" />
+              </button>
             </li>
-            <li className="author-date-container">
-              <Link to={ `/@${username}` }
-                  className="green-button">
+            <li className="author-date-container search-user-text">
+              <button onClick={ this.handleNavigate.bind(this) }
+                  className="user-search-btn">
                 {name}
-              </Link>
+              </button>
+              { this.renderDescription() }
             </li>
-            { this.renderDescription() }
           </ul>
         </div>
       </li>
@@ -43,4 +55,12 @@ class UserIndexItem extends Component {
   }
 }
 
-export default UserIndexItem;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUser: (username) => (dispatch(fetchUser(username)))
+  }
+}
+
+export default withRouter(connect(
+  null,
+  mapDispatchToProps)(UserIndexItem));
