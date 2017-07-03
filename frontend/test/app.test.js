b41cfa9826ca39017ad7630c807b1c9abae3ef6f
Brian Jang <brian.jang91@gmail.com>
2017-07-03 16:54:34 -0400

    Add babelrc and create the first test

--- frontend/test/app.test.js
+++ frontend/test/app.test.js
@@ -1,2 +1,10 @@
 import React from "react";
 import App from "../components/app";
+import { shallow } from "enzyme";
+
+describe("App Component'", () => {
+  it("contains 'hello' text", () => {
+    const app = shallow(<App/>);
+    expect(app.contains(<div>hello</div>)).toBe(true);
+  });
+});
